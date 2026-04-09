6d22ddcbf8bfc0cf7f56bc6cc128ee46afafd798
Niber236 <tonmail@gmail.com>
2026-04-10 00:03:36 +0200

    task0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,7 +2,7 @@
 """
 this module provides a type-annoted fuction to add two floats.
 """
-def add(a: flaot, b: float) -> float:
+def add(a: flaot, b: float) ->  float:
     """
     summs two float  numbers and returns the result.
     """
