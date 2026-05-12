da7593845d3f9eed445d42c6fd7948dbf848883b
Niber236 <tonmail@gmail.com>
2026-05-12 23:07:57 +0200

    fuu-task

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,42 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((fields) => {
+        let output = 'This is the list of our students\n';
+        const sortedFields = Object.keys(fields).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+        for (const field of sortedFields) {
+          output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        }
+        response.status(200).send(output.trim());
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(process.argv[2])
+      .then((fields) => {
+        if (fields[major]) {
+          response.status(200).send(`List: ${fields[major].join(', ')}`);
+        } else {
+          response.status(200).send('List: ');
+        }
+      })
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+export default StudentsController;
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+    const fields = {};
+
+    for (const line of students) {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    }
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
